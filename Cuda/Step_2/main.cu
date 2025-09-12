92c112b6c1201c16ff94145a0b205d2fce34dfeb
Luc De Mercey <LucLgdm@users.noreply.github.com>
2025-09-12 18:07:19 +0200

    feat(Cuda): complete Step 2 - Vector addition & Memory transfert

---
+++ Cuda/Step_2/main.cu
@@ -0,0 +1,69 @@
+// ============================================================================
+//  FILENAME   : main.cu
+//  PROJECT    : GPU Rendering Playground
+//  AUTHOR     : Luc <lucdemercey@gmail.com>
+//  CREATED    : 2025-09-12
+//  UPDATED    : 2025-09-12
+//  DESCRIPTION: Step 2 Cuda - Vector Addition & Memory Transfers
+// ============================================================================
+
+#include <iostream>
+using namespace std;
+
+#include <cuda_runtime.h>
+
+__global__ addVector(const float *A, const float *B, float *C, int N) {
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (ids < N)
+		C[idx] = A[idx] + B[idx]; 
+}
+
+int main() {
+	int N = 1 << 20; // one million elements
+	size_t size = N * sizeof(float);
+
+	// CPU Memory, h for host
+	float *h_A = new float[N];
+	float *h_B = new float[N];
+	float *h_C = new float[N];
+
+	 // Initialize vectors
+	for (int i = 0; i < N; ++i) {
+		h_A[i] = i * 0.5f;
+		h_B[i] = i * 2.0f;
+	}
+
+	// GPU Memory, d for device
+	float *d_A, *d_B, *d_C;
+	cudamalloc(d_A, size);
+	cudamalloc(d_B, size);
+	cudamalloc(d_C, size);
+
+	// Copy from CPU to GPU
+	cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+
+	// Kernel lunch
+	int threadPerBlock = 256;
+	int blocksPerGrid = (N + threadPerBlock - 1) / threadPerBlock; // In the end n block + n2 threads on one block
+
+	addVector<<<blocksPerGrid, threadPerBlock>>>(d_A, d_B, d_C, N);
+
+	cudaDeviceSynchronize();
+
+	// Copy memory from GPU to CPU
+	cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost)
+
+	// Check result
+	cout << "h_C[0] = " << h_C[0] << ", h_c[N - 1] = " << h_C[N - 1] << endl;
+
+	// Free memory
+	cudaFree(d_A);
+	cudaFree(d_B);
+	cudaFree(d_C);
+	delete[] h_A;
+	delete[] h_B;
+	delete[] h_C;
+
+	return 0;
+}

35b54fb1efe12eaa77fb7f92edea68d188d7a1b2
Luc De Mercey <LucLgdm@users.noreply.github.com>
2025-10-17 16:04:55 +0200

    feat(Fusion): Makefile, organisation

---
+++ Fusion/Step_1/srcs/main.cpp
@@ -0,0 +1,16 @@
+// ============================================================================
+//  FILENAME   : main.cpp
+//  PROJECT    : GPU Rendering Playground
+//  AUTHOR     : Luc <lucdemercey@gmail.com>
+//  CREATED    : 2025-10-17
+//  UPDATED    : 2025-10-17
+//  DESCRIPTION: Fusion : Step 1 - 
+// ============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello world!" << endl;
+	return 0;
+}
